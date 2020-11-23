b174ec89b61a120844c9bcae85f081e26a9eb6b4
Kexi Dang <dangkexi@gmail.com>
2020-11-22 21:42:57 -0600

    chore update

--- demo/state-management.js
+++ demo/state-management.js
@@ -58,3 +58,108 @@ store.dispatch({
   type: 'DECREMENT',
   payload: 1
 })
+
+// React useReducer（when states get huge）
+/* 
+  const [something, setSomething] = useState('apple')
+  --->
+  const [state, dispatch] = useReducer(reducer, { something: 'apple' })
+*/
+function useMySpecialReducer() {
+  const [state, dispatch] = useReducer(
+    (state, action) => {
+      switch (action.type) {
+        case "TOGGLE_DARK_MODE": {
+          return {
+            ...state,
+            darkMode: !state.darkMode
+          };
+        }
+        case "NAME_CHARACTER": {
+          return {
+            ...state,
+            name: action.name,
+            error:
+              action.name.length > 15
+                ? "That name is way too long, bucko"
+                : null
+          };
+        }
+        case "RANDOM_VALUES": {
+          return {
+            ...state,
+            name: randomName()
+          };
+        }
+        case "DISMISS_ERROR": {
+          return { 
+            ...state,
+            error: null
+          };
+        }
+        default: {
+          return state;
+        }
+      }
+    },
+    {
+      darkMode: false,
+      name: "",
+      error: null
+    }
+  );
+  return [state, dispatch];
+}
+
+export default function App() {
+  const [
+    { darkMode, name, error },
+    dispatch
+  ] = useMySpecialReducer();
+
+  return (
+    <div className={`App ${darkMode ? "darkmode" : ""}`}>
+      <button
+        onClick={() => {
+          dispatch({ type: "TOGGLE_DARK_MODE" });
+        }}
+      >
+        Dark Mode {darkMode ? "ON" : "OFF"}
+      </button>
+      <br />
+      <input
+        type="text"
+        placeholder="Type your name"
+        value={name}
+        onChange={event => {
+          dispatch({
+            type: "NAME_CHARACTER",
+            name: event.target.value
+          });
+        }}
+      />
+      {error && (
+        <div className="error">
+          {error}
+          <button
+            onClick={() => {
+              dispatch({ type: "DISMISS_ERROR" });
+            }}
+          >
+            Dismiss
+          </button>
+        </div>
+      )}
+      <div className="sheet">
+        <h2>Name: {name}</h2>
+      </div>
+      <button
+        onClick={() => {
+          dispatch({ type: "RANDOM_VALUES" });
+        }}
+      >
+        Do it all for me instead
+      </button>
+    </div>
+  );
+}
